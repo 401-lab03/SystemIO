50605208d5d798e1f35bce41daede21fdec55e83
Allyson Reyes <areyes986@gmail.com>
2020-03-18 18:00:05 -0700

    commented out our plan for different methods and tests

---
+++ Lab03SytemIO/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using Lab03SytemIO;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ItemIsAppended()
+        {
+            // make sure append is added to list
+            // string newstring = Program.append(string)
+            // Assert
+           
+
+        }
+
+        [Fact]
+        public void ItemIsRemoved()
+        {
+
+        }
+
+        
+    }
+}
