300605485601c8da7702ed41eb504377b7076743
Allyson Reyes <areyes986@gmail.com>
2020-03-19 11:27:50 -0700

    structure for testing

--- Lab03SytemIO/XUnitTestProject1/UnitTest1.cs
+++ Lab03SytemIO/XUnitTestProject1/UnitTest1.cs
@@ -7,21 +7,49 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void ItemIsAppended()
+        public void ItemsAreAppendedTest1()
         {
-            // make sure append is added to list
-            // string newstring = Program.append(string)
-            // Assert
-           
+            //string items = "bananas";
+            //string blah = (method name)File.AppendAllLines("../../../list.txt", items)
+            //Assert.IsTrue(list.txt.Includes(items));
+        }
+
+        [Fact]
+        public void ItemIsRemovedTest1()
+        {
+            string[] items = new string[] { "water", "toilet paper", "bananas" };
+            string[] reducedItem = Program.methodname();
+            Assert.Equals(Array.length - 1, items);
 
         }
 
         [Fact]
-        public void ItemIsRemoved()
+        public void ItemisAddedTest1()
         {
-
+            string[] items = new string[] { "water", "toilet paper", "bananas" };
+            string[] reducedItem = Program.methodname();
+            Assert.Equals(Array.length + 1, items);
         }
 
-        
+        [Fact]
+        public void ListIsCreatedTest1()
+        {
+            string[] items = new string [];
+            File.WriteAllText(path, items)
+        }
+
+
+        //[TestMethod()]
+        //public void WriteMovieListToFileTest1()
+        //{
+        //    Movie movie1 = new Movie("Title", "Genre", "Actor", "Year");
+        //    movieSystem.AddMovie(movie1);
+        //    movieSystem.WriteMovieListToFile("MovieListTEST.txt");
+        //    var fileText = File.ReadLines("MovieListTEST.txt");
+        //    Assert.IsTrue(fileText.ToString().Length > 1);
+        //}
+
+
+
     }
 }
