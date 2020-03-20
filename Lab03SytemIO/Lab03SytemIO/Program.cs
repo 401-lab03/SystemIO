7045ff2ad9d0b3b1112417917ae2edd04bd9bd98
Allyson Reyes <areyes986@gmail.com>
2020-03-19 20:25:12 -0700

    added in comments in the code and finished readme

--- Lab03SytemIO/Lab03SytemIO/Program.cs
+++ Lab03SytemIO/Lab03SytemIO/Program.cs
@@ -7,13 +7,17 @@ namespace Lab03SytemIO
     {
         public static string path = "../../../list.txt";
 
+        /// <summary>
+        /// The Main methods populates the entire interface menu and calls the other methods when user chooses an option.
+        /// </summary>
         public static void Main()
         {
 
-            Console.WriteLine("Welcome to your COVID grocery list!");           
+            Console.WriteLine("Welcome to your COVID-19 grocery list!");           
             Console.WriteLine("Press 1 to view your list");
             Console.WriteLine("Press 2 to add an item to your list");
-            Console.WriteLine("Press 3 to delete an item");
+            Console.WriteLine("Press 3 to delete an item"); 
+            Console.WriteLine("Press 4 to Exit");
 
             string userInput = Console.ReadLine();
 
@@ -31,6 +35,8 @@ namespace Lab03SytemIO
 
             if (userInput == "1")
             {
+                Console.Clear();
+                Console.WriteLine("Your List: ");
                 ReadLines();
                 ReadFile();
                 Main();
@@ -43,6 +49,8 @@ namespace Lab03SytemIO
                 string userAdd = Console.ReadLine();
                 string[] convertToArray = new string[] {userAdd};
                 AddToList(convertToArray);
+                Console.WriteLine(" ");
+                Console.WriteLine("Great! Here's your updated list: ");
                 ReadFile();
                 Main();
             }
@@ -50,22 +58,38 @@ namespace Lab03SytemIO
             {
                     Console.Clear();
                     Console.WriteLine("What item would you like to delete?");
+                    Console.WriteLine(" ");
+                    ReadFile();
                     string userDelete = Console.ReadLine();
                     DeleteItem(userDelete);
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Great! Here's your updated list: ");
                     ReadFile();
                     Main();
             }
+            if (userInput == "4")
+            {
+                Console.WriteLine("Thank you, Goodbye!");
+                Environment.Exit(0);
+            }
         }
-
+        
+        /// <summary>
+        /// If the txt is not created, this method create one.
+        /// </summary>
+        /// <param name="items"> The list items the user enters </param>
+        /// <returns> The list of items </returns>
         public static string[] CreateAFile (string[] items)
         {
             string path = "../../../list.txt";
             File.WriteAllLines(path, items);
-            Console.WriteLine(items);
             return items;
 
         }
 
+        /// <summary>
+        /// This method simply reads the file and displays the list.
+        /// </summary>
         static void ReadFile()
         {
             string path = "../../../list.txt";
@@ -73,6 +97,10 @@ namespace Lab03SytemIO
             Console.WriteLine(listArray);
         }
 
+        /// <summary>
+        /// It reads all list items and returns the items.
+        /// </summary>
+        /// <returns> The user's list items </returns>
         static string [] ReadLines()
         {
             string path = "../../../list.txt";
@@ -80,6 +108,10 @@ namespace Lab03SytemIO
             return myList;
         }
 
+        /// <summary>
+        /// This method allows users to add to their list.
+        /// </summary>
+        /// <param name="addToList"> The items the user wants to add to the list. </param>
         static void AddToList(string [] addToList)
         {
             string path = "../../../list.txt";
@@ -94,6 +126,10 @@ namespace Lab03SytemIO
 
         }
 
+        /// <summary>
+        /// Allows user to delete from the list. 
+        /// </summary>
+        /// <param name="deleteItem"> The item the user wants to delete from list. </param>
         static void DeleteItem(string deleteItem)
         {
             string[] currentList = ReadLines();
@@ -120,16 +156,6 @@ namespace Lab03SytemIO
             }
             CreateAFile(newList);
         }
-
-
-        // Create 2 txt files, one to catch errors, the other to view
-
-        // interface
-
-        // test
-
-        //test 1
-
 
 
     }
